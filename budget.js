e23a8d9b9bfe21a51a750205515f895cc6d4b346
Ardash55 <ardash30042005g@gmail.com>
2025-04-11 14:53:39 +0300

    вернулся к проекту, сделал класс счета и методы мзменения названия и суммы

--- budget.js
+++ budget.js
@@ -4,42 +4,11 @@ class Budget {
         this.many = many;
     }
 
-    add(value) {
-        this.many += value;
-        return this.many;
+    setMany(value) {
+        this.many = this.many + value;
     }
 
-    take(value) {
-        this.many -= value;
-        return this.many;
-    }
-
-    change(value) {
-        this.many = value;
-        return this.many;
-    }
-
-    rename(newName) {
+    setName(newName) {
         this.name = newName;
-        return this.name;
     }
 }
-
-let categories = {
-
-}
-
-function addCategory(name) {
-    categories[name] = 0;
-    return categories;
-}
-
-// let cash = new Budget('Наличные', 500);
-
-console.log(categories);
-
-addCategory('Еда');
-addCategory('Транспорт');
-addCategory('Жилье');
-
-console.log(categories);
