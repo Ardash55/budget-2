83a580986236bee479c874e221b8a98e3c3d2701
Ardash55 <ardash30042005g@gmail.com>
2025-04-12 13:55:55 +0300

    Добавил общий див для всех счетов (пока пустой) и массив для них же (пока без автодобавления)

--- budget.js
+++ budget.js
@@ -12,3 +12,15 @@ class Budget {
         this.name = newName;
     }
 }
+
+let allAccounts = [];
+
+let card = new Budget('Карта', 0);
+let cash = new Budget('Наличные', 300);
+
+allAccounts.push(card);
+allAccounts.push(cash);
+
+console.log(allAccounts);
+
+let accountsConteiner = document.getElementById('accounts');
